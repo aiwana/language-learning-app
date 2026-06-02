3613b6e93736909576fc42ccebbea60aef2b9ee8
aiwana <85691475+aiwana@users.noreply.github.com>
2026-06-02 15:05:08 +0700

    Create placeholder project for further development

---
+++ WebShadowing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShadowing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
