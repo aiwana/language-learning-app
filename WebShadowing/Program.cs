a22730b3aa2bad021c25e49a462d218d8da263cf
minhanhdangtapcode <heonho2205@gmail.com>
2026-07-07 03:44:16 +0700

    feat: build Courses page with 3 sections
    Closes #42

--- WebShadowing/Program.cs
+++ WebShadowing/Program.cs
@@ -1,14 +1,24 @@
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 using WebShadowing.Data;
 using WebShadowing.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ICourseService, CourseService>();
 builder.Services.AddScoped<ILessonContentService, LessonContentService>();
+
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(
+        Path.Combine(builder.Environment.ContentRootPath, "App_Data", "DataProtectionKeys")))
+    .SetApplicationName("WebShadowing");
 
 builder.Services.AddControllersWithViews();
 
